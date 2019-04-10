1dc439cfdffc2e7a5bbda3d0e0bcda2480415f5d
AndrewJGaut <ajg@umail.ucsb.edu>
2019-04-09 20:44:05 -0700

    another bug fix

--- DBPedia/ParseDBPedia.py
+++ DBPedia/ParseDBPedia.py
@@ -16,29 +16,32 @@ Postcondition:
     returns value for that attribute for that person.
 '''
 def getAttributeForPerson(person_name, attribute):
-    person_name = formatName(person_name)
-    person_json = requests.get('http://dbpedia.org/data/' + person_name + '.json').json()
-    person_data = person_json['http://dbpedia.org/resource/' + person_name]
     try:
-        person_attr = person_data['http://dbpedia.org/ontology/' + attribute][0]['value']
-    except:
+        person_name = formatName(person_name)
+        person_json = requests.get('http://dbpedia.org/data/' + person_name + '.json').json()
+        person_data = person_json['http://dbpedia.org/resource/' + person_name]
         try:
-            person_attr = person_data['http://xmlns.com/foaf/0.1/' + attribute][0]['value']
+            person_attr = person_data['http://dbpedia.org/ontology/' + attribute][0]['value']
         except:
             try:
-                person_attr = person_data['http://dbpedia.org/property/' + attribute][0]['value']
+                person_attr = person_data['http://xmlns.com/foaf/0.1/' + attribute][0]['value']
             except:
                 try:
-                    person_attr = person_data['http://www.w3.org/1999/02/22-rdf-syntax-ns#' + attribute][0]['value']
+                    person_attr = person_data['http://dbpedia.org/property/' + attribute][0]['value']
                 except:
                     try:
-                        person_attr = person_data['http://purl.org/linguistics/gold/' + attribute][0]['value']
+                        person_attr = person_data['http://www.w3.org/1999/02/22-rdf-syntax-ns#' + attribute][0]['value']
                     except:
-                        return 'ERROR: could not find attribute'
+                        try:
+                            person_attr = person_data['http://purl.org/linguistics/gold/' + attribute][0]['value']
+                        except:
+                            return 'ERROR: could not find attribute'
 
-    if('/' in person_attr or '_' in person_attr):
-        person_attr = getNameFromUrl(person_attr)
-    return person_attr
+        if('/' in person_attr or '_' in person_attr):
+            person_attr = getNameFromUrl(person_attr)
+        return person_attr
+    except:
+        return 'ERROR: could not find attribute'
 '''
 Preconditions:
     url is a DBPedia url with a name at the end
