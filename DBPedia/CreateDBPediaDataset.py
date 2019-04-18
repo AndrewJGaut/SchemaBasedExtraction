8c9e69bba67145874757214833d2201f158e0446
EC2 Default User <ec2-user@ip-172-31-36-80.us-east-2.compute.internal>
2019-04-18 05:47:52 +0000

    updated createdatset script

--- DBPedia/CreateDBPediaDataset.py
+++ DBPedia/CreateDBPediaDataset.py
@@ -37,11 +37,15 @@ def createDataset(person_file_path, attribs, dataset_name, browser):
     # get file to read names from
     person_file = open(person_file_path, 'r')
 
+    # elinimate issues with saving file name later
+    person_file_path = person_file_path.replace('/', '_')
+
     #count rows in workbook so that we know where to write data
     row_counter = 1
 
     for line in person_file.readlines():
         name = formatName(line.strip())
+        print(name)
         curr_person_attribs = list()
         write = True
         for attrib in attribs:
@@ -188,61 +192,6 @@ def getSentences(browser, person_name, attrib_vals):
     return attribs_2_sentences
 
 
-
-'''
-NOTE: this is more efficient that just sorting by a single hypernym each time
-Precondition:
-    person_file_path is the name of a file with person names for one gender
-    ***hypernyms is a LIST of categories the person fits into
-    attribs is a list of strings that represent the attributes we want to put in the database
-    dataset_name is the name of your dataset wihtout file extension
-Postcondition:
-    Creates an Excel spreadsheet with columns:
-    PersonName Attribute1 Attribute2 ... AttributeN
-    and with all entries filled in
-def createDatasetSortByHypernyms(person_file_path, hypernym, attribs, dataset_name):
-    # create excel spreadsheet
-    dataset = xlwt.Workbook()
-    dataset_sheet = dataset.add_sheet('data')
-    dataset_sheet.write(0, 0, "Full Name")
-    for i in range(len(attribs)):
-        dataset_sheet.write(0, (i+1), attribs[i])
-
-
-
-    # get file to read names from
-    person_file = open(person_file_path, 'r')
-
-    #count rows in workbook so that we know where to write data
-    row_counter = 1
-
-    for line in person_file.readlines():
-        print("processing person: " + str(line))
-        name = formatName(line.strip())
-        curr_person_attribs = list()
-        write = True
-        for attrib in attribs:
-            curr_person_attrib = getAttributeForPerson(name, attrib)
-            if(curr_person_attrib == "ERROR: could not find attribute"):
-                # if this is true, then this person doesn't have all the attributes we want; thus, we discard this data point
-                write = False
-                break
-            curr_person_attribs.append(getAttributeForPerson(name, attrib))
-
-        if(write):
-            #now, write person to excel sheet
-            dataset_sheet.write(row_counter, 0, line.strip())
-            for i in range(len(curr_person_attribs)):
-                dataset_sheet.write(row_counter, (i+1), curr_person_attribs[i])
-            row_counter += 1
-
-        # just testing it out for now
-        if(row_counter >= 200):
-            break
-    dataset.save('AttributeDatasets/' + hypernym + "_" + dataset_name + ".xls")
-'''
-
-
 if __name__ == '__main__':
     print('running')
 
@@ -250,8 +199,8 @@ if __name__ == '__main__':
     options = Options()
     options.add_argument("--headless")
     browser = webdriver.Chrome(chrome_options=options)
-    createDataset(sys.argv[1], ['hypernym', 'spouse', 'birthDate', 'birthPlace'], 'sent_test', browser)
-    #createDataset("PersonData_ttl/male_names.txt", ['hypernym', 'spouse', 'birthDate', 'birthPlace'], 'test')
+    #createDataset(sys.argv[1], ['hypernym', 'spouse', 'birthDate', 'birthPlace'], 'sent_test', browser)
+    createDataset(sys.argv[1], ['hypernym', 'spouse', 'birthDate', 'birthPlace'], 'nohypernym_full_train', browser)
     #createDatasetSortByHypernym("PersonData_ttl/male_names.txt", 'Politican', ['party', 'religion', 'predecessor'], 'test_h')
     #createDatasetSortByHypernym("PersonData_ttl/male_names.txt", 'Player', ['weight', 'team','position', 'number'], 'test_h2')
     print(sys.argv[1])
