9d566049a492d0493fd4cfcdf65296a458c8981d
AndrewJGaut <ajg@umail.ucsb.edu>
2019-05-07 23:18:12 -0700

    added code to calc Fleiss Kappa and Krippendorff's Alpha

--- ParseAMTData/AnnotaterAgreement.py
+++ ParseAMTData/AnnotaterAgreement.py
@@ -1,6 +1,27 @@
 import xlrd
-import statsmodels.stats.inter_rater
+import numpy as np
+from krippendorff_alpha import *
+#import statsmodels.stats.inter_rater
 
+
+
+def fleiss_kappa(M):
+  """
+  See `Fleiss' Kappa <https://en.wikipedia.org/wiki/Fleiss%27_kappa>`_.
+  :param M: a matrix of shape (:attr:`N`, :attr:`k`) where `N` is the number of subjects and `k` is the number of categories into which assignments are made. `M[i, j]` represent the number of raters who assigned the `i`th subject to the `j`th category.
+  :type M: numpy matrix
+  """
+  N, k = M.shape  # N is # of items, k is # of categories
+  n_annotators = float(np.sum(M[0, :]))  # # of annotators
+
+  p = np.sum(M, axis=0) / (N * n_annotators)
+  P = (np.sum(M * M, axis=1) - n_annotators) / (n_annotators * (n_annotators - 1))
+  Pbar = np.sum(P) / N
+  PbarE = np.sum(p * p)
+
+  kappa = (Pbar - PbarE) / (1 - PbarE)
+
+  return kappa
 
 def getAgreement(dataset_path):
     dataset = xlrd.open_workbook(dataset_path)
@@ -53,11 +74,14 @@ def getFleissKappa(dataset_path):
 
     #get fleiss kappa of each set of three annotators
     for i in range(1, sheet.nrows, 3):
-        ratings = [['' for x in range(sheet.ncols)] for y in range(3)]
+        ratings = np.zeros([3, sheet.ncols], dtype=int)
         for curr_row in range(0, 3):
             for j in range(0, sheet.ncols):
-                ratings[curr_row][j] = sheet.cell_value(i + curr_row, j)
-        f_kappas.append(statsmodels.stats.inter_rater.fleiss_kappa(ratings))
+                curr_val = 0
+                if sheet.cell_value(i + curr_row, j) == 'yes':
+                    curr_val = 1
+                    ratings[curr_row][j] = curr_val
+        f_kappas.append(fleiss_kappa(ratings))
 
     # get average fleiss kappa value
     ave = 0
@@ -66,10 +90,43 @@ def getFleissKappa(dataset_path):
     return float( float(ave) / float(len(f_kappas)))
 
 
+def getKripAlpha(dataset_path):
+    dataset = xlrd.open_workbook(dataset_path)
+    sheet = dataset.sheet_by_index(0)
+
+    alphas = list()
+
+    for i in range(1, sheet.nrows, 3):
+        ratings = np.zeros([3, sheet.ncols], dtype=int)
+        for curr_row in range(0, 3):
+            for j in range(0, sheet.ncols):
+                curr_val = 0
+                if sheet.cell_value(i + curr_row, j) == 'yes':
+                    curr_val = 1
+                    ratings[curr_row][j] = curr_val
+        alphas.append(krippendorff_alpha(ratings))
+
+    # get average fleiss kappa value
+    ave = 0
+    for num in alphas:
+        ave += num
+    return float( float(ave) / float(len(alphas)))
+
 
 if __name__ == '__main__':
-    #getAgreement('Batch_3631670_batch_results.xls')
-    #getAgreement('batch_cheap_answers.xlsx')
     #getAgreement('FullStudyResults.xls')
-    getFleissKappa('FullStudyResults.xls')
+    #print(getFleissKappa('Datasets/FullStudyResults.xls'))
+    #print(getKripAlpha('Datasets/FullStudyResults.xls'))
 
+    '''
+    dataset = xlrd.open_workbook("Datasets/test_please_tell_not_true.xlsx")
+    sheet = dataset.sheet_by_index(0)
+
+    for i in range(1, sheet.nrows, 3):
+        #ratings = np.zeros([3, sheet.ncols], dtype=str)
+        ratings = [['' for x in range(sheet.ncols)] for y in range(3)]
+        for curr_row in range(0, 3):
+            for j in range(0, sheet.ncols):
+                ratings[curr_row][j] = sheet.cell_value(i + curr_row, j)
+        print(ratings)
+    '''
